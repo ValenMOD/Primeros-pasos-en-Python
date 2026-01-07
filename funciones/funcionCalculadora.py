174154afb4491bf2baad5686304dad599ced5e19
ValenMOD <torrespochito08@gmail.com>
2026-01-07 12:52:04 +0100

    Add files via upload

---
+++ funciones/funcionCalculadora.py
@@ -0,0 +1,32 @@
+def suma(num1, num2):
+    
+    resultado = num1 + num2
+    print(resultado)
+    
+suma(5, 4)
+    
+    
+def resta(num1, num2):
+    
+    resultado = num1 - num2
+    print(resultado)
+
+    
+resta(9, 2)
+
+    
+def multiplica(num1, num2):
+    
+    resultado = num1 * num2
+    print(resultado)
+
+multiplica(3, 2)
+
+    
+def divide(num1, num2):
+    
+    resultado = num1 / num2
+    resultado = int(resultado) #Esto convierte al resultado de la división en un número entero.
+    print(resultado)
+    
+divide(8, 2)
